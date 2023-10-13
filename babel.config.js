5b5a7c9c4b1d12b6a7ed0517faf1ee6cf7b10f46
Diego Cardenas <diego.cardenasaleg@gmail.com>
2023-10-13 09:22:09 -0500

    added alias config to handle imports correctlly

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,23 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            adapters: "./src/adapters",
+            components: "./src/components",
+            constants: "./src/constants",
+            interfaces: "./src/interfaces",
+            navigation: "./src/navigation",
+            screens: "./src/screens",
+            services: "./src/services",
+            state: "./src/state",
+            utils: "./src/utils"
+          },
+        },
+      ],
+    ],
   };
 };
