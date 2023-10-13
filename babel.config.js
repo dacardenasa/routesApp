7208e44360dc9bd772233cabcf3dd452018a0cbc
Diego Cardenas <diego.cardenasaleg@gmail.com>
2023-10-13 10:07:04 -0500

    added config to handle alias in App

--- babel.config.js
+++ babel.config.js
@@ -6,16 +6,18 @@ module.exports = function(api) {
       [
         "module-resolver",
         {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            adapters: "./src/adapters",
-            components: "./src/components",
-            constants: "./src/constants",
-            interfaces: "./src/interfaces",
-            navigation: "./src/navigation",
-            screens: "./src/screens",
-            services: "./src/services",
-            state: "./src/state",
-            utils: "./src/utils"
+            "@adapters": "./src/adapters",
+            "@components": "./src/components",
+            "@constants": "./src/constants",
+            "@interfaces": "./src/interfaces",
+            "@navigation": "./src/navigation",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@state": "./src/state",
+            "@utils": "./src/utils"
           },
         },
       ],
